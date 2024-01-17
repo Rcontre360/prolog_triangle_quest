45edfe8b6f848269aee37ea313d530b50e5e3439
rafaelChelo <rafael@chelofinance.io>
2024-01-16 23:59:13 -0400

    feat: move function

--- main.pl
+++ main.pl
@@ -1,9 +1,20 @@
 % Define the triangle levels
 triangleLevel([1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5]).
-isEmpty([false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]).
+isEmpty([false,false,false,false,true,false,false,false,false,false,false,false,false,false,false]).
+
+modifyAtIndex([], _, _, []).
+modifyAtIndex([_|Rest], 1, Value, [Value|Rest]).
+modifyAtIndex([X|Rest], Index, Value, [X|ResTail]) :-
+    Index > 1,
+    NewIndex is Index - 1,
+    modifyAtIndex(Rest, NewIndex, Value, ResTail).
+
+modifyPuzzle(Index, Value, X):-
+    triangleLevel(Levels),  
+    modifyAtIndex(Levels,Index,Value, X).
 
 % Predicate to check the difference between two numbers levels
-levelDifference(Cur, Nxt, Diff) :-
+isLevelDifference(Cur, Nxt, Diff) :-
     triangleLevel(Levels),  
     nth1(Cur, Levels, LevelCur),  
     nth1(Nxt, Levels, LevelNxt),  
@@ -25,7 +36,7 @@ isValidMove(Cur, Nxt) :-
 
     triangleLevel(Levels),  
     nth1(Cur, Levels, LevelCur),  
-    levelDifference(Cur, Nxt, LevelDiff),  
+    isLevelDifference(Cur, Nxt, LevelDiff),  
 (
     (Nxt =:= Cur - 2*LevelCur + 1, LevelDiff = 2);  %arriba a la izq
     (Nxt =:= Cur - 2*LevelCur + 3, LevelDiff = 2); %arriba a la derecha
@@ -33,6 +44,19 @@ isValidMove(Cur, Nxt) :-
     (Nxt =:= Cur + 2, LevelDiff = 0); % derecha
     (Nxt =:= Cur + 2*LevelCur + 1, LevelDiff = 2); % abajo a la izquierda
     (Nxt =:= Cur + 2*LevelCur + 3, LevelDiff = 2) % abajo a la derecha
+).
+
+isIntermediate(Cur, Nxt, Inter) :- 
+    triangleLevel(Levels),  
+    nth1(Cur, Levels, LevelCur),  
+    isLevelDifference(Cur, Nxt, LevelDiff),  
+(
+    (Nxt =:= Cur - 2*LevelCur + 1, LevelDiff = 2, Calc is Cur - LevelCur, Inter = Calc);  %arriba a la izq
+    (Nxt =:= Cur - 2*LevelCur + 3, LevelDiff = 2, Calc is Cur - LevelCur + 1, Inter = Calc); %arriba a la derecha
+    (Nxt =:= Cur - 2, LevelDiff = 0, Calc is Cur - 1, Inter = Calc); % izquierda
+    (Nxt =:= Cur + 2, LevelDiff = 0, Calc is Cur + 1, Inter = Calc); % derecha
+    (Nxt =:= Cur + 2*LevelCur + 1, LevelDiff = 2, Calc is Cur + LevelCur, Inter = Calc); % abajo a la izquierda
+    (Nxt =:= Cur + 2*LevelCur + 3, LevelDiff = 2, Calc is Cur + LevelCur + 1, Inter = Calc) % abajo a la derecha
 ).
 
 % Predicate to find all valid moves
@@ -50,17 +74,25 @@ isSolved([Cur | Rest],IsOneFilled) :-
         Cur = true, IsOneFilled = false, isSolved(Rest,true)
     ).
 
+move(From,To, Puzzle, NxtPuzzle) :-
+    isValidMove(From,To),
+    isIntermediate(From,To,Inter),
+    modifyAtIndex(Puzzle, From, true, FirstChange),
+    modifyAtIndex(FirstChange, To, false, SecondChange),
+    modifyAtIndex(SecondChange, Inter, true, NxtPuzzle).
     
-%canBeSolved([],_):- false.
-%canBeSolved([CurPos, NxtPos], Puzzle) :-
-    %allValidMoves(CurPos, ValidMoves),
-    %(
-        %canBeSolved(ValidMoves)
-        %;
-        %canBeSolved(NxtPos)
-    %).
+canBeSolved([],_):- false.
+canBeSolved([CurPos, NxtPos], Puzzle) :-
+    allValidMoves(CurPos, ValidMoves),
+    modifyAtIndex(Puzzle, CurPos, true, NxtPuzzle),
+    (
+        canBeSolved(ValidMoves,NxtPuzzle)
+        ;
+        canBeSolved(NxtPos, NxtPuzzle)
+    ).
 
-
-
+main(From,To,X) :-
+    isEmpty(Puzzle),
+    move(From,To,Puzzle,X).
 
 
