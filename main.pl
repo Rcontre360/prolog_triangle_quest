ced6b6be557ae016b360b2783118b10864fbeb68
rafaelChelo <rafael@chelofinance.io>
2024-01-16 21:19:50 -0400

    feat: test

--- main.pl
+++ main.pl
@@ -1,28 +1,69 @@
-% Define parent-child relationships.
-parent(john, doug).
-parent(pam, doug).
-parent(tom, liz).
-parent(pam, liz).
+triangle_level([1, 2, 2, 3, 3, 3, 4, 4, 4, 4,5,5,5,5,5])
+is_empty([false,false,false,false,false,false,false,false,false,false,false,false,false,false,false])
 
-% Define male and female.
-male(john).
-male(doug).
-female(pam).
-female(liz).
+is_valid_move(cur, nxt) :- 
+    nxt == cur - triangle_level[cur - 1] 
+    ;
+    nxt == cur - triangle_level[cur - 1] + 1
+    ;
+    nxt == cur - 1
+    ;
+    nxt == cur + 1
+    ;
+    nxt == cur + triangle_level[cur - 1]
+    ;
+    nxt == cur + triangle_level[cur - 1] + 1
 
-% Define a rule for 'father'.
-father(Father, Child) :- 
-    male(Father),
-    parent(Father, Child).
 
-% Define a rule for 'mother'.
-mother(Mother, Child) :- 
-    female(Mother),
-    parent(Mother, Child).
+is_solved([], is_one_active) :- is_one_active
+is_solved([cur | nxt], is_one_active) :- 
+    (
+        cur == false,
+        is_solved(nxt,is_one_active)
+    )
+    ;
+    (
+        cur == true,
+        is_one_active == false,
+        is_solved(nxt,true)
+    )
+    
+is_valid_path(cur, is_empty_arr) :-
+    is_solved(is_empty_arr)
+    ;
+    (
+        is_valid_path(cur - triangle_level[cur]),
+    )
+    ;
+    (
+        is_valid_path(cur - triangle_level[cur] + 1),
+    )
+    ;
+    (
+        is_valid_path(cur - triangle_level[cur] + 1),
+    )
+    ;
+    (
+        is_valid_path(nxt == cur - 1),
+    )
+    ;
+    (
+        is_valid_path(nxt == cur + 1),
+    )
+    ;
+    (
+        is_valid_path(cur + triangle_level[cur]),
+    )
+    ;
+    (
+        is_valid_path(cur + triangle_level[cur] + 1)
+    )
 
-% Define a rule for 'sibling'.
-sibling(Person1, Person2) :- 
-    parent(Parent, Person1),
-    parent(Parent, Person2),
-    Person1 \= Person2.
+
+
+
+
+
+
+
 
